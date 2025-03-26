d7a604422b4b1d45623094264d00cdbb7eb4d361
JanzenDC <janzendelacruz28@gmail.com>
2025-03-26 22:47:06 +0800

    Update Brevo API key in forgot_password and user_management scripts

--- backend/user_management.php
+++ backend/user_management.php
@@ -15,7 +15,7 @@ use Brevo\Client\Api\TransactionalEmailsApi;
 use Brevo\Client\Model\SendSmtpEmail;
 
 // Configure API key for Brevo
-$config = Configuration::getDefaultConfiguration()->setApiKey('api-key', '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');
+$config = Configuration::getDefaultConfiguration()->setApiKey('api-key', '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');
 
 // Create Brevo API instance
 $apiInstance = new TransactionalEmailsApi(
